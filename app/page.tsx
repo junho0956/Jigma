af4968ccb92ec8e74ea11c2b4eb49e20dfd654f1
junho0956 <junho0956@naver.com>
2024-06-02 23:51:20 +0900

    add sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import LeftSideBar from "@/components/LeftSideBar";
+import RightSideBar from "@/components/RightSideBar";
 
 export default function Page() {
   return (
@@ -9,7 +11,9 @@ export default function Page() {
       <Navbar />
       
       <section className="flex h-full flex-row">
+        <LeftSideBar />
         <Live />
+        <RightSideBar />
       </section>
     </main>
   );
